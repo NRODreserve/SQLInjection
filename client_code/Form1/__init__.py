784f9ca726c3cd3ff8b159e6775b035fe690c05e
noah.rodriguez-el-hardouzi@student.htl-rankweil.at <noah.rodriguez-el-hardouzi@student.htl-rankweil.at>
2024-12-18 11:28:10 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,3 +12,21 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    # Überprüfen, ob die Textbox leer ist
+    if not self.text_box_1.text.strip():  # strip() entfernt Leerzeichen
+        # Fehlermeldung anzeigen, wenn die Textbox leer ist
+        alert("Bitte füllen Sie das Textfeld aus!", title="Fehler")
+    else:
+        # Weiter mit der Anmeldung
+        username = self.text_box_1.text
+        # Hier kannst du die Anmelde-Logik hinzufügen
+        print(f"Benutzername eingegeben: {username}")
+
+
